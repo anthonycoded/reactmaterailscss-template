29e42ffef494023f529a1aa152c0c4c64d843c5f
shawn barbel <barbels33@gmail.com>
2021-12-07 11:58:12 -0500

    added router, redux store, MuiTheme provider

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
+import { Provider } from "react-redux";
+import store from "./store/store";
 
-import React, { Component} from "react";
-import "./App.css";
+import Header from "./components/UI/Header";
+import LandingPage from "./components/LandingPage";
+import Footer from "./components/UI/Footer";
+import Login from "./components/auth/Login";
+import Theme from "./config/theme";
 
-class App extends Component{
-  render(){
-    return(
-      <div className="App">
-        <h1> Hello, World! </h1>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <ThemeProvider theme={Theme}>
+      <Provider store={store}>
+        <Router>
+          <Header className="display-block"></Header>
+          <Switch>
+            <Route path="/" exact>
+              <LandingPage></LandingPage>
+            </Route>
+            <Route path="/admin" exact component={Admin}></Route>
+            <Route path="/login" exact component={Login}></Route>
+          </Switch>
+          <Footer></Footer>
+        </Router>
+      </Provider>
+    </ThemeProvider>
+  );
 }
 
 export default App;
