83399fa32eadca918c0cf7fb743a0266dd96e2e0
shawn barbel <barbels33@gmail.com>
2021-12-07 23:08:32 -0500

    added lodash and hot reload

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import _ from "lodash";
 import App from "./App.js";
 ReactDOM.render(<App />, document.getElementById("root"));
+module.hot.accept();
