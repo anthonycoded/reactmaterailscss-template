6ec9b7e215b5a1ba3bb4ddf4fa53ca58dcc95833
shawn barbel <barbels33@gmail.com>
2021-12-07 23:07:07 -0500

    removed unused import

--- src/store/store.js
+++ src/store/store.js
@@ -2,13 +2,11 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 
-import { songReducer } from "./reducers/songReducer";
 import { authReducer } from "./reducers/authReducer";
 
 const middleware = [thunk];
 
 const rootReducer = combineReducers({
-  songs: songReducer,
   auth: authReducer,
 });
 
